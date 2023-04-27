86bffe9061873417ca87f787b5df5c688dfbf264
juliostmendes <juliostmendes@outlook.com>
2023-04-27 10:28:26 -0300

    Aplicativo android

---
+++ Desenvolvimento/3.Implementacao/app/app/src/main/java/com/example/projetosapc/FormLogin.java
@@ -0,0 +1,14 @@
+package com.example.projetosapc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
